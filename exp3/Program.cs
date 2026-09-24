f861e9a291d3ac940df6a7007dee1355fb34134b
vengateshpv <vengateshpv14@gmail.com>
2026-09-24 12:27:30 +0530

    Add files via upload

---
+++ exp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Number
+{
+    public int Value;
+    public Number(int value)
+    {
+        Value = value;
+    }
+    public static Number operator +(Number n1, Number n2)
+    {
+        return new Number(n1.Value + n2.Value);
+    }
+    public void Display()
+    {
+        Console.WriteLine("Result: " + Value);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Number num1 = new Number(10);
+        Number num2 = new Number(20);
+        Number sum = num1 + num2;
+        sum.Display(); 
+     }
+}
