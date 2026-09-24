08ecd1d17a79d9f702cfb47a8a3b250e51cf1bfe
vengateshpv <vengateshpv14@gmail.com>
2026-09-24 12:24:41 +0530

    Add files via upload

---
+++ exp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Student
+{
+    public string Name;
+    public int Age;
+    public void Display()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.Name = "John";
+        s1.Age = 20;
+        s1.Display();
+    }
+}
