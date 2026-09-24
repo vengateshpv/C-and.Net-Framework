01e7a9d56df6259aed3a2e05c2d16098787e536d
vengateshpv <vengateshpv14@gmail.com>
2026-09-24 12:29:22 +0530

    Add files via upload

---
+++ exp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public delegate void Notify();
+class Publisher
+{
+    public event Notify OnNotify;
+    public void RaiseEvent()
+    {
+        Console.WriteLine("Event is raised.");
+        OnNotify?.Invoke();
+    }}
+class Subscriber
+{
+    public void ShowMessage()
+    {
+        Console.WriteLine("Event received successfully.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Publisher pub = new Publisher();
+        Subscriber sub = new Subscriber();
+        pub.OnNotify += sub.ShowMessage;
+        pub.RaiseEvent();
+    }
+}
+
